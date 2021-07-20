952e9ce54c1e7198b087dbbbedcd9380b2bd66a5
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-20 12:40:49 +0300

    Update script.py

--- bot/script.py
+++ bot/script.py
@@ -1,6 +1,17 @@
-from translate import Translator
+def get_text(start_text: str) -> str:
+    translated_text = start_text
+    # Detect the language
+    from langdetect import detect
+    from_lang = detect(start_text)
+
+    language_codes = [from_lang, 'es', 'uk', 'vi', 'tr', from_lang]
+
+    for index in range(len(language_codes) - 1):
+        from translate import Translator
+        translator = Translator(from_lang=language_codes[index], to_lang=language_codes[index+1])
+        translated_text = translator.translate(translated_text)
+
+    return translated_text
 
 
-translator = Translator(to_lang="uk")
-translation = translator.translate("Hello, dad")
-print(translation)
+print(get_text('Утер Светоносный'))
