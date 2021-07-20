4e5cec6dee1856ec958bdfdd4e7bc2495ad557bf
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-20 18:03:25 +0300

    Some updates
    Uninstall translate

--- bot/script.py
+++ bot/script.py
@@ -4,17 +4,4 @@ def get_translated_text(start_text: str) -> str:
     :param start_text: input text
     :return: translated text
     """
-    translated_text = start_text
-    # Detect the language
-    from langdetect import detect
-    from_lang = detect(start_text)
-
-    language_codes = [from_lang, 'es', 'uk', 'vi', 'tr', from_lang]
-
-    for index in range(len(language_codes) - 1):
-        from translate import Translator
-        translator = Translator(from_lang=language_codes[index], to_lang=language_codes[index+1])
-        translated_text = translator.translate(translated_text)
-    if translated_text.startswith('MYMEMORY'):
-        return 'The service is not available now'
-    return translated_text
+    return start_text
